2b388f93351448334b508dee7161785ba7bcedc6
Morten Bendeke <mbendeke@hotmail.com>
2022-10-31 20:31:28 +0100

    lessio 05 klaret

--- src/components/Workshop.jsx
+++ src/components/Workshop.jsx
@@ -1,9 +1,23 @@
-export default function Workshop() {
+import { useParams } from "react-router-dom";
+import { getWorkshop } from "../api";
+
+
+export default function Workshop()
+ {
+    const obj = useParams()
+    const getAWorkShop = getWorkshop(obj.workId)
+    console.log(getAWorkShop)
 
     return (
         <>
-            <h2>Workshop Sessions</h2>
-            <p>Workshop session content displays here...</p>
+            <h2>{getAWorkShop.name}</h2>
+            <ul className="session-list">
+                {getAWorkShop.sessions.map(session =>
+                <li><p className="session-name">{session.name}</p><p>{session.speaker.name}</p><p>{session.speaker.org}</p></li>
+                
+                
+            )}
+            </ul>
         </>
     );
 }
